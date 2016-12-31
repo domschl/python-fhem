6f4b30ffad7200295b3a7712f8a51b33c489d843
Dominik Schloesser <dsc@dosc.net>
2016-12-31 21:40:21 +0100

    Initial version

---
+++ fhem/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fhem',
+      version='0.1',
+      description='Python API for FHEM home automation server',
+      url='http://github.com/domschl/python-fhem',
+      author='Domschl',
+      author_email='dom@dosc.net',
+      license='MIT',
+      packages=['fhem'],
+      zip_safe=False)
