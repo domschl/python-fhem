f06efd9893a65ba2b1843353b10d6e38b13c806e
Dominik Schloesser <dsc@dosc.net>
2017-01-01 10:27:34 +0100

    more pypi stuff

--- fhem/setup.py
+++ fhem/setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='fhem',
       version='0.1',
       description='Python API for FHEM home automation server',
+      long_description='Uses socket communication to set and get states from FHEM home automation server with optional SSL encryption and password support',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+      ],
+      keywords='funniest joke comedy flying circus',
       url='http://github.com/domschl/python-fhem',
-      author='Domschl',
-      author_email='dom@dosc.net',
+      author='Dominik Schloesser',
+      author_email='dsc@dosc.net',
       license='MIT',
       packages=['fhem'],
       zip_safe=False)
