33660e11183912c67d45b1ff58aee2e0adad081f
Dominik Schloesser <dsc@dosc.net>
2017-01-01 13:01:46 +0100

    version minor incr.

--- fhem/setup.py
+++ fhem/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fhem',
-      version='0.1.1',
+      version='0.1.2',
       description='Python API for FHEM home automation server',
       long_description='Uses socket communication to set and get states from FHEM home automation server with optional SSL encryption and password support',
       classifiers=[
