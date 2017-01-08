6d0d896c30b8c53df3f3799c662df8d94cc2cba2
Dominik Schloesser <dsc@dosc.net>
2017-01-08 18:05:44 +0100

    setup.py version classifier fix

--- fhem/setup.py
+++ fhem/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='fhem',
-      version='0.2.0',
+      version='0.2.1',
       description='Python API for FHEM home automation server',
       long_description='Uses socket or http(s) communication to set and get states from FHEM home automation server with optional SSL encryption and password support',
       classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
       ],
       keywords='fhem home automation',
