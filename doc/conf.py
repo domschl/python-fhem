3df9233402b411b6c8181e13c65133bce231f538
Dominik Schlösser <dsc@dosc.net>
2019-09-26 14:23:00 +0200

    Doc update for version 0.6.3

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'Dominik Schlösser'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.6.2'
+release = '0.6.3'
 
 
 # -- General configuration ---------------------------------------------------
